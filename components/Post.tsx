b23c0d7cee1b94ecb2e5257d066ce7c9900fe8e5
AndShir93 <andshir93@gmail.com>
2021-02-26 18:14:08 +0400

    blog

--- components/Post.tsx
+++ components/Post.tsx
@@ -16,10 +16,11 @@ export type PostProps = {
 const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   const authorName = post.author ? post.author.name : "Unknown author";
   return (
-    <div onClick={() => Router.push("/p/[id]", `/p/${post.id}`)}>
+    <div style={{cursor:'pointer'}} onClick={() => Router.push("/p/[id]", `/p/${post.id}`)}>
       <h2>{post.title}</h2>
       <small>By {authorName}</small>
       <ReactMarkdown source={post.content} />
+      <small>{post.author.email}</small>
       <style jsx>{`
         div {
           color: inherit;
